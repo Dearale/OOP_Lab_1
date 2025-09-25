d8a38aaaeeca7dff37b17888bf105c58a22ee1bb
Dearale <sidiuschannel@gmail.com>
2025-09-25 10:11:39 +0300

    solution 2

--- src/converter.cpp
+++ src/converter.cpp
@@ -1,12 +1,13 @@
 #include "../include/converter.h"
 
 std::string convert(int hours, int minutes, std::string period) {
-    if (period == "pm" && hours != 12) {
-        hours += 12;
-    } else if (period == "am" && hours == 12) {
-        hours = 0;
+    if (period == "pm") {
+        if (hours == 12)
+            hours = 0;
+        else if (hours != 12)
+            hours += 12;
     }
 
-    return (hours < 10 ? "0" : "") + std::to_string(hours) + ":" +
+    return (hours < 10 ? "0" : "") + std::to_string(hours) +
             (minutes < 10 ? "0" : "") + std::to_string(minutes);
 }
--- test/tests01.cpp
+++ test/tests01.cpp
@@ -2,27 +2,27 @@
 #include "../include/converter.h"
 
 TEST(test_01, basic_test_set) {
-    ASSERT_TRUE(convert(8, 30, "am")=="08:30");
+    ASSERT_TRUE(convert(8, 30, "am")=="0830");
 }
 
 TEST(test_02, basic_test_set) {
-    ASSERT_TRUE(convert(8, 30, "pm")=="20:30");
+    ASSERT_TRUE(convert(8, 30, "pm")=="2030");
 }
 
 TEST(test_03, basic_test_set) {
-    ASSERT_TRUE(convert(12, 30, "am")=="00:30");
+    ASSERT_TRUE(convert(12, 30, "am")=="1230");
 }
 
 TEST(test_04, basic_test_set) {
-    ASSERT_TRUE(convert(12, 30, "pm")=="12:30");
+    ASSERT_TRUE(convert(12, 30, "pm")=="0030");
 }
 
 TEST(test_05, basic_test_set) {
-    ASSERT_TRUE(convert(4, 59, "pm")=="16:59");
+    ASSERT_TRUE(convert(4, 59, "pm")=="1659");
 }
 
 TEST(test_06, basic_test_set) {
-    ASSERT_TRUE(convert(3, 1, "pm")=="15:01");
+    ASSERT_TRUE(convert(3, 1, "pm")=="1501");
 }
 
 int main(int argc, char **argv) {
